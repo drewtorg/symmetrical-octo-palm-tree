2b137ffa1866f0a1071756e6f61cefa3597b9a41
Drew <drew.torgeson@gmail.com>
2016-04-11 19:02:06 -0600

    Finalize classifier and test data

--- naive_bayes_yelp.py
+++ naive_bayes_yelp.py
@@ -2,31 +2,38 @@ import nltk
 import simplejson as json
 from sklearn.naive_bayes import MultinomialNB
 from textstat.textstat import textstat
+import pickle
 
+# use textstat to pull out useful features from the text
 def get_features(text):
     output = {}
     output['text'] = text
     output['syllables'] = textstat.syllable_count(text)
     output['lexicons'] = textstat.lexicon_count(text)
     output['sentences'] = textstat.sentence_count(text)
-    # output['flesch'] = textstat.flesch_reading_ease(text)
     output['smog'] = textstat.smog_index(text)
     return output
 
-not_useful = {}
-useful = {}
+# pickles the classifier for future use, this will improve speeds when classfying user input
+def save_classifier(classifier):
+    with file('my_classifier.pickle', 'wb') as f:
+        pickle.dump(classifier, f, -1)
+
+# unpickles the classifier
+def load_classifier():
+    with file('my_classifier.pickle', 'rb') as f:
+        return pickle.load(f)
 
 with file('output.txt') as f:
     data = json.loads(f.read())
 
+# extract the features from each line of data
 featuresets = [(get_features(data[i]['text']), str(data[i]['useful'])) for i in range(len(data))]
 
+n = len(featuresets) / 2
+train_set = featuresets[:n]
+test_set = featuresets[n:]
 
-print featuresets[0]
-
-train_set = featuresets[:50]
-test_set = featuresets[50:]
-
+# train the classifier on the first half and test on the second half
 cl = nltk.NaiveBayesClassifier.train(train_set)
 print "%.3f" % nltk.classify.accuracy(cl, test_set)
-print cl.classify({'text':'Is this useful?'}) # get a confidence for the prediction
